33baf19583fefae1c9b1490384fc371ae5045780
Hasibul321 <hasibulalam192@gmail.com>
2023-10-10 02:08:07 +0600

    First commit on feature branch

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello{
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int a = 10,b=5;
+        int sum = a+b;
+        System.out.println("The sum is = "+sum);
     }
 }
